7e3a1171d5bfa650509cdda49c7ce018abe69c4a
GarethCott <gareth.cott@gmail.com>
2023-07-04 16:52:56 +0200

    Fix toast error by adding toaster provider in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/ui/ThemeProvider'
 import { ModalProvider } from '@/providers/ModalProvider'
+import { ToasterProvider } from '@/providers/ToastProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <html lang="en">
           <body className={inter.className}>
             {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
+              <ToasterProvider/>
               <ModalProvider/>
                 {children}
             {/* </ThemeProvider> */}
