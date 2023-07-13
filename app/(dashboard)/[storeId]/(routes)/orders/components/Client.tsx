7436c94cd83fe8197b97eeb82ed306af5af3d2eb
GarethCott <gareth.cott@gmail.com>
2023-07-13 15:01:15 +0200

    Add orders page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/Client.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+
+import Heading from "@/components/ui/Heading"
+import { Separator } from "@/components/ui/Separator"
+
+
+import { FC } from 'react'
+import { OrderColumn, columns } from "./Columns"
+import { DataTable } from "@/components/ui/DataTable"
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+const OrderClient: FC<OrderClientProps> = ({data}) => {
+
+    return (
+        <>
+            <Heading title={`Orders (${data.length})`} description="Manage orders for your store"/>
+            <Separator/>
+            <DataTable searchKey="products" columns={columns} data={data}/>
+        </>
+    )
+}
+
+export default OrderClient
